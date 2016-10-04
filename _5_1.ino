b527266f108736760c725d397da3c4650ebfb8ab
irmadewita11 <irmadewita123@gmail.com>
2016-10-04 15:09:31 +0700

    Add files via upload

---
+++ _5_1.ino
@@ -0,0 +1,19 @@
+void setup ()
+{
+  Serial.begin (9600);
+}
+void loop()
+{
+  Serial.println("Hello World\r\n");
+  delay (2000);
+  Serial.println("Halo\r\n");
+  delay (2000);
+  Serial.println("Apa Kabar semua\r\n");
+  delay (2000);
+  Serial.println("kabar\r\n");
+  delay (2000);
+  Serial.println("Semua\r\n");
+  delay (2000);
+  Serial.println("Yang\r\n");
+  delay (2000);
+}
